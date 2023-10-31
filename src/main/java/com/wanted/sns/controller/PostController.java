be6554ebf1de4562ba67988fce0cc6f3742be33d
kade <bhpark93@gmail.com>
2023-11-01 04:28:52 +0900

    refactor : 컨트롤러 리턴타입 수정

--- src/main/java/com/wanted/sns/controller/PostController.java
+++ src/main/java/com/wanted/sns/controller/PostController.java
@@ -3,6 +3,8 @@ package com.wanted.sns.controller;
 import com.wanted.sns.dto.PostResponse;
 import com.wanted.sns.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,9 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/{id}")
-    public PostResponse getPost(@PathVariable("id") long seq) {
-        return postService.getPost(seq);
+    public ResponseEntity<PostResponse> getPost(@PathVariable("id") long seq) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(postService.getPost(seq));
     }
 
 }
