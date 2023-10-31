4d9ff964ce2f5c6c85ffd41a8a264e2f0f311db9
kade <bhpark93@gmail.com>
2023-11-01 03:39:19 +0900

    test : restAssured 의존성 추가, 단위 테스트 수정, 통합 테스트 추가

--- src/test/java/com/wanted/sns/repository/PostRepositoryTest.java
+++ src/test/java/com/wanted/sns/repository/PostRepositoryTest.java
@@ -3,26 +3,22 @@ package com.wanted.sns.repository;
 import com.wanted.sns.domain.Order;
 import com.wanted.sns.dto.PostRequest;
 import com.wanted.sns.dto.PostResponse;
-import jakarta.persistence.EntityManager;
 import java.util.List;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@Transactional
-@SpringBootTest
+@DataJpaTest
 public class PostRepositoryTest {
 
     @Autowired
     PostRepository postRepository;
 
-    @Autowired
-    EntityManager entityManager;
-
+    @DisplayName("게시글 목록 조회 테스트")
     @Test
-    public void getPosts() {
+    public void getPostList() {
         String searchBy = "title";
         String search = "1";
         String hashtag = "해시태그1";
